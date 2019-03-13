7f9c84e05fb43338d7f041084a3a10ef07cabd8f
ocdev <yoseph@docotel.com>
2019-03-13 14:43:20 +0800

    initial commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+<head>
+    @include('layouts.partial.meta')
+    
+    <title>Education</title>
+
+    @include('layouts.partial.styles')
+    
+    </head>
+    <body>	
+        <header id="header" id="home">
+            @include('layouts.partial.header')
+        </header><!-- #header -->
+
+        @yield('content')
+                    
+        <!-- start footer Area -->		
+        <footer class="footer-area section-gap">
+            @include('layouts.partial.footer')
+        </footer>	
+        <!-- End footer Area -->	
+
+
+        @include('layouts.partial.script')
+
+        @yield('js')
+    </body>
+</html>
